96ce655c05d8745f7e163ab440339dfed2222c31
alemarostica <alemarostica@virgilio.it>
2022-12-12 18:34:59 +0100

    added express.json middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express =  require('express');
 const mongoose = require('mongoose');
 const app=express();
+app.use(express.json());
 
 mongoose.connect(
     process.env.DB_TOKEN,
