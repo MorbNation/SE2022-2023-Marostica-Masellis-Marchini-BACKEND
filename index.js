7ebe5d22ae138412c80b97cccef0a2d0cbf5e1f7
alemarostica <alemarostica@virgilio.it>
2022-12-12 15:54:30 +0100

    setup workspace

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const dotenv = require('dotenv');
+require('dotenv').config();
+const express=require('express');
+const app=express();
+
+app.get("/", (req, res) => {
+    res.send(req.headers, req.originalUrl, req.method, req.body);
+});
+
+app.listen(3000, () => {
+    console.log(`App listening on port 3000`);
+});
