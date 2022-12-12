7043217c42afc493ce1904d7d8c5132beda7209b
alemarostica <alemarostica@virgilio.it>
2022-12-12 21:54:21 +0100

    fixed formatting

--- controllers/post.js
+++ controllers/post.js
@@ -4,7 +4,7 @@ const newPost = (req, res) => {
     console.log(req.body);
     console.log('Trying to add new post, checking if a post with the same title exists...');
     Post.findOne({ title: req.body.title }, (err, data) => {
-        if(!data){
+        if (!data) {
             const newPost = new Post({
                 title: req.body.title,
                 description: req.body.description,
@@ -13,12 +13,12 @@ const newPost = (req, res) => {
             });
 
             newPost.save((err, data) => {
-                if(err) return res.json({Error: err});
+                if (err) return res.json({ Error: err });
                 return res.json(data);
             });
         } else {
-            if(err) return res.json(`Something went wrong. ${err}`);
-            return res.json({message: "Post already exists"});
+            if (err) return res.json(`Something went wrong. ${err}`);
+            return res.json({ message: "Post already exists" });
         }
     });
 };
@@ -27,8 +27,8 @@ const getPost = (req, res) => {
     console.log("Listing all Posts...");
     //gets all posts
     Post.find({}, (err, data) => {
-        if(err) {
-            return res.json({ Error: err});
+        if (err) {
+            return res.json({ Error: err });
         }
         return res.json(data);
     });
@@ -37,17 +37,17 @@ const getPost = (req, res) => {
 const deletePost = (req, res) => {
     console.log(req.params);
     let postTitle = req.params.title;
-    var query = { title: postTitle};
+    var query = { title: postTitle };
     console.log(`Deleting post ${postTitle}...`);
 
     Post.deleteOne(query, (err, collection) => {
-        if(err) {
+        if (err) {
             throw err;
         } else {
             console.log(`Post ${postTitle} deleted succesfully.`);
-            res.json({ message: "DELETE Post"});
+            res.json({ message: "DELETE Post" });
         }
     });
 };
 
-module.exports = {newPost, getPost, deletePost};
+module.exports = { newPost, getPost, deletePost };
