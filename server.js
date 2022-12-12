7873d49f3409d3b31d25b7f299c9e8c40a68a09d
alemarostica <alemarostica@virgilio.it>
2022-12-12 16:41:11 +0100

    mongodb works nice

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express=require('express');
+require('dotenv').config();
+const express =  require('express');
+const mongoose = require('mongoose');
 const app=express();
+
+mongoose.connect(
+    process.env.DB_TOKEN,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) return console.log("Error: ", err);
+        console.log("MongoDB Connection -- Ready state is: ", mongoose.connection.readyState);
+    }
+);
 
 app.get("/", (req, res) => {
     res.send(req.headers, req.originalUrl, req.method, req.body);
