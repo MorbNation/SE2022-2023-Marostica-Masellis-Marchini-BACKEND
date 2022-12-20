96d094f04ae89f4095469c16dde8cadf2f18ff97
alemarostica <alemarostica@virgilio.it>
2022-12-20 15:34:08 +0100

    token generation works

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,18 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const generateToken = async (req, res) => {
+    await console.log(req.body);
+
     let user = await User.findOne({ email: req.body.email }).exec();
+
+    await console.log(user);
 
     if (!user) {
         console.log("User not found.");
         res.json({ success: false, message: "Authentication failed, user not found" });
     }
 
-    if (user.password != req.body.password) {
+    if (user.psw != req.body.psw) {
         res.json({ success: false, message: "Authentication failed, wrong password" });
     }
 
