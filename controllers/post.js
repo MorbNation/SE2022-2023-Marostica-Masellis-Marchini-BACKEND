ad1305ff33eb91528121e7c03d8e75c3a536871a
alemarostica <alemarostica@virgilio.it>
2022-12-20 13:03:44 +0100

    actually working duplicate check

--- controllers/post.js
+++ controllers/post.js
@@ -1,33 +1,59 @@
 const Post = require('../models/post');
-
-const newPost = (req, res) => {
+const newPost = async (req, res) => {
     console.log(req.body);
     console.log('Trying to add new post, checking if a post with the same title exists...');
-    Post.findOne({ where: { title: req.body._id } }, (err, data) => {
-        if (!data) {
-            const newPost = new Post({
-                id: req.body.id,
-                title: req.body.title,
-                date: req.body.req,
-                text: req.body.text,
-                media: req.body.media,
-                tag: req.body.tag,
-                punteggio_post: req.body.punteggio_post,
-                segnalato: req.body.segnalato,
-                numero_commenti: req.body.numero_commenti,
-                associato_a_contest: req.body.associato_a_contest,
-                creatore_post: req.body.creatore_post
-            });
 
-            newPost.save((err, data) => {
-                if (err) return res.json({ Error: err });
-                return res.json(data);
-            });
-        } else {
-            if (err) return res.json(`Something went wrong. ${err}`);
-            return res.json({ message: "Post already exists." });
-        }
-    });
+    //I don't know why, I don't want to know why but for some reason mongodb will not return a valid body to compare
+    // Post.findOne({ where: { id: req.body.id } }, (err, data) => {
+    //     if (!data) {
+    //         const newPost = new Post({
+    //             id: req.body.id,
+    //             title: req.body.title,
+    //             date: req.body.req,
+    //             text: req.body.text,
+    //             media: req.body.media,
+    //             tag: req.body.tag,
+    //             punteggio_post: req.body.punteggio_post,
+    //             segnalato: req.body.segnalato,
+    //             numero_commenti: req.body.numero_commenti,
+    //             associato_a_contest: req.body.associato_a_contest,
+    //             creatore_post: req.body.creatore_post
+    //         });
+
+    //         newPost.save((err, data) => {
+    //             if (err) return res.json({ Error: err });
+    //             return res.json(data);
+    //         });
+    //     } else {
+    //         if (err) return res.json(`Something went wrong. ${err}`);
+    //         return res.json({ message: "Post already exists." });
+    //     }
+    // });
+
+    let post = await Post.findOne({ id: req.body.id }).exec();
+
+    if (!post) {
+        const newPost = new Post({
+            id: req.body.id,
+            title: req.body.title,
+            date: req.body.req,
+            text: req.body.text,
+            media: req.body.media,
+            tag: req.body.tag,
+            punteggio_post: req.body.punteggio_post,
+            segnalato: req.body.segnalato,
+            numero_commenti: req.body.numero_commenti,
+            associato_a_contest: req.body.associato_a_contest,
+            creatore_post: req.body.creatore_post
+        });
+
+        newPost.save((err, data) => {
+            if (err) return res.json({ Error: err });
+            return res.json(data);
+        });
+    } else {
+        return res.json({ message: "Post already exists", req: req.body })
+    }
 };
 
 const getPost = (req, res) => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,41 +1,71 @@
 const { collection } = require('../models/user');
 const User = require('../models/user');
 
-const newUser = (req, res) => {
+const newUser = async (req, res) => {
     console.log(req.body);
     console.log("Trying to register new user...");
-    User.findOne({ where: { username: req.body.username } }, (err, data) => {
-        console.log(data);
-        if (!data) {
-            const newUser = new User({
-                username: req.body.username,
-                email: req.body.email,
-                psw: req.body.psw,
-                description: "Write a description here.",
-                proIcon: "placeholder icon path",
-                nsfwIconFlag: req.body.nsfwIconFlag,
-                banner: "placeholder banner path",
-                nsfwBannerFlag: req.body.nsfwBannerFlag,
-                userscore: 0,
-                lingua: req.body.lingua,
-                isAdmin: false,
-                nsfwSetting: req.body.nsfwSetting,
-                theme: "black",
-                followed_users: req.body.followed_users,
-                favourites: req.body.favourites,
-                timer: 0,
-                token: "Ciao"
-            });
+    // User.findOne({ where: { "username": req.body.username } }, (err, data) => {
+    //     if (!data) {
+    //         const newUser = new User({
+    //             username: req.body.username,
+    //             email: req.body.email,
+    //             psw: req.body.psw,
+    //             description: "Write a description here.",
+    //             proIcon: "placeholder icon path",
+    //             nsfwIconFlag: req.body.nsfwIconFlag,
+    //             banner: "placeholder banner path",
+    //             nsfwBannerFlag: req.body.nsfwBannerFlag,
+    //             userscore: 0,
+    //             lingua: req.body.lingua,
+    //             isAdmin: false,
+    //             nsfwSetting: req.body.nsfwSetting,
+    //             theme: "black",
+    //             followed_users: req.body.followed_users,
+    //             favourites: req.body.favourites,
+    //             timer: 0,
+    //             token: "Ciao"
+    //         });
 
-            newUser.save((err, data) => {
-                if (err) return res.json({ Error: err });
-                return res.json(data);
-            });
-        } else {
-            if (err) return res.json(`Something went wrong. ${err}`);
-            return res.json({ message: "User already exists." })
-        }
-    });
+    //         newUser.save((err, data) => {
+    //             if (err) return res.json({ Error: err });
+    //             return res.json(data);
+    //         });
+    //     } else {
+    //         if (err) return res.json(`Something went wrong. ${err}`);
+    //         return res.json({ message: "User already exists.", req: req.body })
+    //     }
+    // });
+
+    let user = await User.findOne({ username: req.body.username }).exec();
+
+    if (!user) {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            psw: req.body.psw,
+            description: "Write a description here.",
+            proIcon: "placeholder icon path",
+            nsfwIconFlag: req.body.nsfwIconFlag,
+            banner: "placeholder banner path",
+            nsfwBannerFlag: req.body.nsfwBannerFlag,
+            userscore: 0,
+            lingua: req.body.lingua,
+            isAdmin: false,
+            nsfwSetting: req.body.nsfwSetting,
+            theme: "black",
+            followed_users: req.body.followed_users,
+            favourites: req.body.favourites,
+            timer: 0,
+            token: "Ciao"
+        });
+
+        newUser.save((err, data) => {
+            if (err) return res.json({ Error: err });
+            return res.json(data);
+        });
+    } else {
+        return res.json({ message: "User already exists", req: req.body });
+    }
 };
 
 const getUser = (req, res) => {
