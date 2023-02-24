0902ca16bed39a88a8a639afa5528ff00b5748e3
NicoM <65673686+nico-mas@users.noreply.github.com>
2023-02-24 17:21:37 +0100

    Rimosso campo 'timer' non utilizzato

--- models/utente.js
+++ models/utente.js
@@ -21,8 +21,7 @@ const UtenteSchema = new mongoose.Schema({
     nsfw: { type: String, enum: ["no", "blur", "yes"] },
     nome_tema_selezionato: String,
     utenti_seguiti: [String], //forse dovremmo mettere una lista di User
-    post_favoriti: [String],
-    timer: Number
+    post_favoriti: [String]
 });
 
 const Utente = mongoose.model('Utente', UtenteSchema);
