c9ad1905e693fab7c3de0209fa8422180cb68d92
alemarostica <alemarostica@virgilio.it>
2023-05-24 19:27:18 +0200

    API is invoked, page still does not show anything

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -1,16 +1,12 @@
 <script setup>
-import { onMounted, reactive } from 'vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { posts, fetchPosts } from '../states/posts.js'
 
 const HOST = `http://localhost:8080`;
-const posts = reactive([]);
-
-async function showPosts() {
-    posts.values = await (await fetch(HOST + 'api/posts')).json();
-}
+const API_URL = HOST + '/api';
 
 onMounted( () => {
-    showPosts();
+    fetchPosts();
 });
 
 </script>
@@ -19,8 +15,7 @@ onMounted( () => {
     <h1>Post:</h1>
     <ul>
         <li v-for="post in posts.values" :key="post.self">
-            <a :href="HOST+'/api'+post.self">{{ post.id }}</a>
-            -
+            <a :href="HOST+post.self">{{ post.id }}</a>
         </li>
     </ul>
 </template>
---
+++ src/states/posts.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue'
+
+const HOST = import.meta.env.VITE_API_HOST || `http://localhost:8080`;
+const API_URL = HOST + '/api';
+
+const posts =  reactive ([]);
+
+async function fetchPosts() {
+    posts.values = await (await fetch(API_URL + '/posts')).json();
+}
+
+export { posts, fetchPosts };
